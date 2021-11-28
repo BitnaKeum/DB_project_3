e4bf5083c7fd5c345f5bd8b9e10c2c4f3cf97399
YeonGuKang <hsmroof@naver.com>
2021-11-28 22:22:39 +0900

    reserve okay

--- vaccine_reservation/routes/reserve.js
+++ vaccine_reservation/routes/reserve.js
@@ -83,16 +83,13 @@ router.get('/', function(req, res, next) {
         }
 
 
-           // 결과가 제대로 안뽑히는 오류 CLIENT_CODE는 제대로 인데;;
+           
     connection.query(sqlForGetShot,CLIENT_CODE, function(err, rows){
         if(err) console.error("error : " + err);
 
-        console.log("CODE",typeof(CLIENT_CODE))
-        console.log("CODE",typeof(11))
-        console.log("compare : ", 11 == CLIENT_CODE)
 
-
-        if(rows != undefined)
+        console.log("rrrr", rows)
+        if(rows != undefined && rows.length != 0)
         {
             SHOT_DATE_1st = rows[0].SHOT_DATE_1st
             SHOT_DATE_2st = rows[0].SHOT_DATE_2st
@@ -154,6 +151,8 @@ router.get('/', function(req, res, next) {
                 SHOT_DATE_2st:SHOT_DATE_2st,VACCINE_VACCINE_TYPE:VACCINE_VACCINE_TYPE,hospital_name:hospital_name, 
                 VACCINE_ORDER:VACCINE_ORDER,LAST_SHOT_DATE:LAST_SHOT_DATE, MUST_TPYE:MUST_TPYE,
                 MODER_NUM:MODER_NUM, AZ_NUM:AZ_NUM, PHIZER_NUM:PHIZER_NUM});
+
+                connection.release();
            
                 
             });
@@ -168,9 +167,6 @@ router.get('/', function(req, res, next) {
     
     });
 
-    
-    
-    connection.release();
 
 });
 
@@ -196,6 +192,7 @@ router.post('/', function(req, res) {
 
 
         var sqlForGetClient = "SELECT CLIENT_CODE,CLIENT_NAME,AGE FROM CLIENT WHERE ID = ?";
+        var sqlForGetShot = "SELECT SHOT_DATE_1st,SHOT_DATE_2st,VACCINE_VACCINE_TYPE FROM SHOT WHERE CLIENT_CLIENT_CODE =?";
 
         connection.query(sqlForGetClient,id, function(err, rows){
             if(err) console.error("error : " + err);
@@ -208,15 +205,87 @@ router.post('/', function(req, res) {
             
             console.log("here",datas);
 
-            var sqlForInsertMember = "insert into shot(BOOK_DATE_1ST,HOSPITAL_HOSPITAL_CODE,CLIENT_CLIENT_CODE,VACCINE_VACCINE_TYPE) values(?,?,?,?)";
-            connection.query(sqlForInsertMember, datas, function(err, rows){
-                if(err) console.error("err : " + err);
-                console.log("rows: " + JSON.stringify(rows));
+            connection.query(sqlForGetShot,CLIENT_CODE, function(err, rows){
+                if(err) console.error("error : " + err);
+                
+
+                
+                // 기존에 shot 테이블에 들어있는 경우
+                if(rows != undefined && rows.length != 0)
+                {
+                    SHOT_DATE_1st = rows[0].SHOT_DATE_1st
+                    SHOT_DATE_2st = rows[0].SHOT_DATE_2st
+                    VACCINE_VACCINE_TYPE = rows[0].VACCINE_VACCINE_TYPE
+        
+
+                    console.log("rrrre ",SHOT_DATE_1st)
+                    if(SHOT_DATE_1st != undefined)
+                    {
+                        if(SHOT_DATE_2st != undefined)
+                        {
+                            VACCINE_ORDER = '부스터 3';
+                            LAST_SHOT_DATE = SHOT_DATE_2st;
+                        }
+        
+                        else{
+                            VACCINE_ORDER = 2;
+                            LAST_SHOT_DATE = SHOT_DATE_1st;
+                        }
+                    }
+                    else
+                    {
+                        VACCINE_ORDER = 1;
+                        LAST_SHOT_DATE = "해당없음"
+                    }
 
 
+                    if(VACCINE_ORDER != "부스터 3"){
+
+                        
+                        if(SHOT_DATE_1st == null)
+                        {
+                            console.log("imphehe2323")
+                        var sqlForInsertMember = "update (client as c join shot as s) set s.BOOK_DATE_1ST = ? where c.CLIENT_CODE = s.CLIENT_CLIENT_CODE and c.CLIENT_CODE = ?";
+                        connection.query(sqlForInsertMember, [BookDate, CLIENT_CODE], function(err, rows){
+                            if(err) console.error("err : " + err);
+                            console.log("rows: " + JSON.stringify(rows));
+    
+                        });
+                         }
+    
+                        else
+                        {
+                            console.log("imphehe")
+                        var sqlForInsertMember = "update (client as c join shot as s) set s.BOOK_DATE_2ST = ? where c.CLIENT_CODE = s.CLIENT_CLIENT_CODE and c.CLIENT_CODE = ?";
+                        connection.query(sqlForInsertMember, [BookDate, CLIENT_CODE], function(err, rows){
+                            if(err) console.error("err : " + err);
+                            console.log("rows: " + JSON.stringify(rows));
+    
+                        });
+                          }
+                    }
         
         
+                }
+
+
+                // 기존 shot 테이블에 없던 경우
+                else
+                {
+                    var sqlForInsertMember = "insert into shot(BOOK_DATE_1ST,HOSPITAL_HOSPITAL_CODE,CLIENT_CLIENT_CODE,VACCINE_VACCINE_TYPE) values(?,?,?,?)";
+                    connection.query(sqlForInsertMember, datas, function(err, rows){
+                        if(err) console.error("err : " + err);
+                        console.log("rows: " + JSON.stringify(rows));
+
+                    });
+                }
+
+                
+                res.redirect('/shot_info');
+                connection.release();
             });
+
+
 
     
         });
@@ -224,9 +293,6 @@ router.post('/', function(req, res) {
     
     });
 
-
-    res.redirect('/shot_info');
-    connection.release();
     
 });
 
